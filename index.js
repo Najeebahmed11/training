e3e2da13ffc0b09a35cfb3bc332646a1b7f3b86b
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-10 17:51:01 +0500

    Add files via upload
    day 1 code

---
+++ index.js
@@ -0,0 +1,16 @@
+let firstName='najeeb';
+let person={
+        name:'ahmed',
+        age:18
+}
+//dot notation
+person.name='khan';
+//bracket notation
+person['name']='oo';
+        console.log(person)
+let selectedColours=['red','green','blue'];
+console.log(selectedColours);        
+function greet(hello){
+        console.log(hello);
+}
+greet('ahhhh');
