20633bd6985f78e260d70e25efd3f43ea9aeab2b
Najeeb Ahmed <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-14 19:12:35 +0500

    79 debugging tools
    f10 step over
    f11 step into it
    shift+f11 step out

--- Program.cs
+++ Program.cs
@@ -14,14 +14,43 @@ namespace ConsoleApp2
 
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            var path = @"C:\Users\Najeeb Tiwana\source\repos\ConsoleApp2\ConsoleApp2\Program.cs";
+            var numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+            var smallests = GetSmallests(numbers, 3);
 
-            Console.WriteLine(Path.GetExtension(path));
-        } 
+            foreach (var number in smallests)
+                Console.WriteLine(number);
+        }
+
+        public static List<int> GetSmallests(List<int> list, int count)
+        {
+            var smallests = new List<int>();
+
+            while (smallests.Count < count)
+            {
+                var min = GetSmallest(list);
+                smallests.Add(min);
+                list.Remove(min);
+            }
+
+            return smallests;
+        }
+
+        public static int GetSmallest(List<int> list)
+        {
+            // Assume the first number is the smallest
+            var min = list[0];
+            for (var i = 1; i < list.Count; i++)
+            {
+                if (list[i] < min)
+                    min = list[i];
+            }
+            return min;
+        }
     }
 }
+
 
 
 
